69b4bdfef0ad809350a746e0ae125d07c3fa1874
sassamori <mtkmnaerkn@yahoo.co.jp>
2021-04-24 16:02:00 +0900

    postを使ってフォームの利用

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,5 +9,12 @@
 <body>
     <h1>Blade/Index</h1>
     <p>{{$msg}}</p>
+    <form method="POST" action="/hello">
+        @csrf
+        <input type="text" name="msg">
+        <input type="submit">
+    </form>
+
+
 </body>
 </html>
