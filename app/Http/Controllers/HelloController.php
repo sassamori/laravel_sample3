249326905c58aa8b2ca6e58fdcfbc234a04e839d
sassamori <mtkmnaerkn@yahoo.co.jp>
2021-04-24 15:30:20 +0900

    コントローラを利用して表示

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,26 +7,7 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request,Response $response){
-$html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body{font-size:16pt;color:#999;}
-h1{font-size:120pt;text-align:right;color:#fafafa;margin:-50px 0 -120px 0;}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-        $url = $response->status();
-        return $url;
+    public function index(){
+        return view('hello.index');
     }
 }
