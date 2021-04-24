b4cf3a3ff3ef26bca962525a4a11a0022c45f4a0
sassamori <mtkmnaerkn@yahoo.co.jp>
2021-04-24 16:10:24 +0900

    BladeでIFを書いてみる

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,13 +8,15 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>{{$msg}}</p>
-    <form method="POST" action="/hello">
+    @if($msg != '')
+    <p>こんにちは、{{$msg}}さん。</p>
+    @else
+    <p>何か書いてください。</p>
+    @endif
+    <form method="post" action="/hello">
         @csrf
         <input type="text" name="msg">
         <input type="submit">
     </form>
-
-
 </body>
 </html>
