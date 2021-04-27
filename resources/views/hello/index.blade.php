efb3ddb51875c7d30198e0ce1305147da1546844
sassamori <mtkmnaerkn@yahoo.co.jp>
2021-04-27 19:26:03 +0900

    errorディレクティブを使う

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,19 +10,22 @@
 @section('content')
     <p>{{$msg}}</p>
     @if(count($errors)>0)
-    <div>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    </div>
+    <p>入力に問題があります。再入力してください。</p>
     @endif
     <form action="/hello" method="post">
     <table>
         @csrf
+        @error('name')
+        <tr><th>ERROR</th><td>{{$message}}</td></tr>
+        @endif
         <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+        @error('mail')
+        <tr><th>ERROR</th><td>{{$message}}</td></tr>
+        @endif
         <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+        @error('age')
+        <tr><th>ERROR</th><td>{{$message}}</td></tr>
+        @endif
         <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
         <tr><th>send: </th><td><input type="submit" value="send"></td></tr>
     </table>
