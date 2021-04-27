ac7f30354e9dcc6d24dd9a6c1413b4f57a7878d8
sassamori <mtkmnaerkn@yahoo.co.jp>
2021-04-27 18:05:06 +0900

    ミドルウェアのグループを利用

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello','HelloController@index');
+Route::get('hello','HelloController@index')
+    ->middleware('helo');
 Route::post('hello','HelloController@post');
